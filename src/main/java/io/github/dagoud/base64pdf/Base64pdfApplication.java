6ac3f874ef40a0e9bf3323657f037f9e84c569df
David Goudie <dagoudie@Davids-Mac-mini.local>
2024-10-21 23:39:57 +0100

    initial commit w/ working Hello World

---
+++ src/main/java/io/github/dagoud/base64pdf/Base64pdfApplication.java
@@ -0,0 +1,13 @@
+package io.github.dagoud.base64pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Base64pdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Base64pdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dagoud/base64pdf/Base64pdfApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dagoud.base64pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Base64pdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
